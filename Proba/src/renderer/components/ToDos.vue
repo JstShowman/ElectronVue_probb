86d5efee73f3d1e94b146e93a4370f243b9165a9
PathofTesting <43075995+PathofTesting@users.noreply.github.com>
2020-06-06 16:13:49 +0500

    Adding New ElectronVue Projec

---
+++ Proba/src/renderer/components/ToDos.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div>
+            <div>
+                <button @click="fetchTodos()" class="btn btn-primary">Fetch Todos</button>
+            </div>
+        </div>
+        <div>
+            <div>
+                <ul>
+                    <li v-for="todo in todos" :key="todo.id">{{todo.title}}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  const axios = require('axios')
+  export default {
+    name: 'ToDos',
+    data: () => {
+      return {
+        todos: []
+      }
+    },
+    methods: {
+      async fetchTodos () {
+        axios
+          .get('http://localhost:3001/')
+          .then(response => {
+            this.todos = response.data
+          })
+          .catch(error => {
+            if (error) throw new Error(error)
+          })
+      }
+    }
+  }
+</script>
+<style scoped>
+    #fetch-button-row,
+    #todos-row {
+        margin: 10px;
+    }
+</style>
